cd566fa6c0b03798c9db7d62cfcbe251628d3481
SagiBeno <benosagi321@gmail.com>
2026-01-10 09:09:57 +0100

    Remove useless imports

--- messages-app/src/Pages/LoginPage.jsx
+++ messages-app/src/Pages/LoginPage.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { Container, Box, Card, TextField, IconButton, Text, Avatar, Flex, Separator, Button, Spinner } from "@radix-ui/themes";
-import { EyeClosedIcon, EyeOpenIcon } from "@radix-ui/react-icons";
+import { Container, Box, Card, TextField, Text, Avatar, Flex, Button, Spinner } from "@radix-ui/themes";
 import { useNavigate } from "react-router-dom";
 
 import PasswordInput from "../Components/PasswordInput";
