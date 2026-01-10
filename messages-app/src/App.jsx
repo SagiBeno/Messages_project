c7cd2c39beab37ace5b2f1be6ee56925a166260e
SagiBeno <benosagi321@gmail.com>
2026-01-10 12:20:28 +0100

    Add toast

--- messages-app/src/App.jsx
+++ messages-app/src/App.jsx
@@ -2,16 +2,27 @@ import { useState } from 'react';
 import './App.css';
 import LoginPage from './Pages/LoginPage';
 import RegisterPage from './Pages/RegisterPage';
+import AppToast from './Components/Toast';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [showToast, setShowToast] = useState(true);
+  const [toastData, setToastData] = useState( { title: 'Szia', description: 'Szia...' } );
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/' element={<LoginPage loading={loading} setLoading={setLoading} />} />
+        <Route path='/register' element={<RegisterPage loading={loading} setLoading={setLoading} setShowToast={setShowToast} setToastData={setToastData} />} />
       </Routes>
+
+      <AppToast
+        open={showToast}
+        onOpenChange={setShowToast}
+        title={toastData.title}
+        description={toastData.description}
+      />
     </>
   )
 }
