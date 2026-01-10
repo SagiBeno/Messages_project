a9be8a93a2924153542bf198e2baaf67db704c02
SagiBeno <benosagi321@gmail.com>
2026-01-10 18:15:29 +0100

    Modify navbar

--- messages-app/src/Components/Sidebar.jsx
+++ messages-app/src/Components/Sidebar.jsx
@@ -1,19 +1,35 @@
-import { Box, Card, Flex, Tabs, Text, ScrollArea } from "@radix-ui/themes";
+import { Box, Card, Flex, Tabs, Text, ScrollArea, Heading, Avatar } from "@radix-ui/themes";
 
 export default function Sidebar() {
     return (
-        <Card style={{ height: "100%", borderRadius: 0, minWidth: '20vw' }}>
+        <Card style={{ height: "100%", borderRadius: 0, minWidth: '300px' }}>
             <Flex direction="column" style={{ height: "100%" }} gap="3">
-                <Box>
-                    <Text weight="bold" size="4">
-                        Üzenetek
-                    </Text>
-                </Box>
+                <Flex
+                    direction="row"
+                    justify='center'
+                    align='center'
+                >
+                    <Avatar
+                        src="icon.png"
+                        size='3'
+                        title="Mesaj icon"
+                        alt="Mesaj icon"
+                    />
+                    <Heading as="h1" ml='1'>Messaj</Heading>
+                </Flex>
 
-                <Tabs.Root >
-                    <Tabs.List>
+                <Tabs.Root>
+                    <Tabs.List
+                        color='tomato'
+                        style={{
+                            display: "flex",
+                            flexDirection: 'row',
+                            justifyContent: "space-around"
+                        }}
+                    >
                         <Tabs.Trigger value="chats">Csevegések</Tabs.Trigger>
                         <Tabs.Trigger value="friends">Ismerősök</Tabs.Trigger>
+                        <Tabs.Trigger value="searchFriends">Keresés</Tabs.Trigger>
                     </Tabs.List>
                 </Tabs.Root>
 
--- messages-app/src/Pages/ChatsPage.jsx
+++ messages-app/src/Pages/ChatsPage.jsx
@@ -1,45 +1,34 @@
-import { Box, Flex, Card, Text, Button, ScrollArea, Heading } from "@radix-ui/themes";
+import { Box, Flex, Card, Text, Button, ScrollArea } from "@radix-ui/themes";
 import Sidebar from "../Components/Sidebar";
 
 export default function ChatsPage() {
+
   return (
-    <Flex direction="row" height="100vh" width="100vw">
-      <Box
-        style={{
-          width: 320,
-        }}
-        className="sidebar"
-      >
-        <Flex direction="column" style={{ height: "100%" }}>
-          <Box p="3">
-            <Heading>Messaj</Heading>
-          </Box>
+    <Flex direction="row" height="100vh" width="100vw" justify="space-between" align="center">
 
-          <Box style={{ flex: 1, overflow: "hidden" }}>
-            <Sidebar />
-          </Box>
-        </Flex>
-      </Box>
+      <Flex direction="column" style={{ height: "100%" }}>
+        <Box style={{ flex: 1, overflow: "hidden" }} m='5'>
+          <Sidebar />
+        </Box>
+      </Flex>
 
-      <Box
-        style={{
-          width: 'calc(100% - 320px)',
-        }}
-      >
-        <Flex direction="column" style={{ height: "100%" }} mt=''>
+      <Flex direction="column" style={{ height: "100%", width: '100%' }}>
+        <Box style={{ flex: 1, overflow: "hidden" }} m='5'>
+          <Card style={{ height: "100%"}}>
+            <ScrollArea type="auto" scrollbars="vertical" style={{ height: "100%" }}>
 
-          <Box style={{ flex: 1, overflow: "hidden" }}>
-            <Card style={{ height: "100%"}}>
-              <ScrollArea type="auto" scrollbars="vertical" style={{ height: "100%" }}>
-                <Box p="4">
-                  Válassz egy csevegést a bal oldali sávból, vagy kezdj egy újat!
-                </Box>
-              </ScrollArea>
-            </Card>
 
-          </Box>
-        </Flex>
-      </Box>
+              <Flex p="4"
+                justify='center'
+                direction='column'
+                style={{height: '100%', textAlign: 'center'}}
+              >
+                Válasszon egy csevegést a bal oldali sávból, vagy kezdj egy újat!
+              </Flex>
+            </ScrollArea>
+          </Card>
+        </Box>
+      </Flex>
     </Flex>
   )
 }
