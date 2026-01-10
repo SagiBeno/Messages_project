75bd43c18225eb9f6322d1b8797a083576ff2e59
SagiBeno <benosagi321@gmail.com>
2026-01-10 15:17:16 +0100

    Implement login method

--- messages-app/src/App.jsx
+++ messages-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, use } from 'react';
 import './App.css';
 import LoginPage from './Pages/LoginPage';
 import RegisterPage from './Pages/RegisterPage';
@@ -8,12 +8,22 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 function App() {
   const [loading, setLoading] = useState(false);
   const [toastData, setToastData] = useState( { open: false, title: '', description: '', isError: true } );
+  const [userData, setUserData] = useState( { isLoggedIn: false} );
+
+  useEffect( () => {
+    const storedUserData = JSON.parse(localStorage.getItem('userData'));
+    if ( storedUserData ) {
+      setUserData(storedUserData);
+    } else {
+      setUserData( { isLoggedIn: false } );
+    }
+  }, [] );
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<LoginPage loading={loading} setLoading={setLoading} />} />
-        <Route path='/login' element={<LoginPage loading={loading} setLoading={setLoading} />} />
+        <Route path='/' element={<LoginPage loading={loading} setLoading={setLoading} toastData={toastData} setToastData={setToastData} userData={userData} setUserData={setUserData} />} />
+        <Route path='/login' element={<LoginPage loading={loading} setLoading={setLoading} toastData={toastData} setToastData={setToastData} userData={userData} setUserData={setUserData} />} />
         <Route path='/register' element={<RegisterPage loading={loading} setLoading={setLoading} toastData={toastData} setToastData={setToastData} />} />
       </Routes>
 
