cf864daf1a5bac96f1f215385c5710b72eb4cfae
SagiBeno <benosagi321@gmail.com>
2026-01-10 08:59:10 +0100

    Add /register route

--- messages-app/src/App.jsx
+++ messages-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import LoginPage from './Pages/LoginPage';
+import RegisterPage from './Pages/RegisterPage';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Routes>
     </>
   )
