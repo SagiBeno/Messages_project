20a8f68e60698b07354c0ea210744f2a40a5f568
SagiBeno <benosagi321@gmail.com>
2026-01-11 11:48:11 +0100

    Searching for users in the database

--- messages-app/src/Components/Sidebar.jsx
+++ messages-app/src/Components/Sidebar.jsx
@@ -1,44 +1,68 @@
-import { Box, Card, Flex, Tabs, Text, ScrollArea, Heading, Avatar } from "@radix-ui/themes";
+import { Box, Card, Flex, Tabs, Text, ScrollArea, Heading, Avatar, TextField } from "@radix-ui/themes";
 import TabsComponent from "./TabsComponent";
+import SearchNewFriendComponent from "./SearchNewFriendComponent";
 import { useState } from "react";
 
-export default function Sidebar() {
-    const [tabsOptions, setTabsOptions] = useState(
-        {
-            chats: 'Csevegések',
-            friends: 'Ismerősök',
-            search: 'Keresés'
-        }
-    );
-    const [activeTab, setActiveTab] = useState("chats");
+export default function Sidebar({ options, activeTab, setActiveTab, handleSearchNewFriend }) {
 
     return (
-        <Card style={{ height: "100%", borderRadius: 0, minWidth: '300px' }}>
-            <Flex direction="column" style={{ height: "100%" }} gap="3">
-                <Flex
-                    direction="row"
-                    justify='center'
-                    align='center'
-                >
-                    <Avatar
-                        src="icon.png"
-                        size='3'
-                        title="Mesaj icon"
-                        alt="Mesaj icon"
-                    />
-                    <Heading as="h1" ml='1'>Messaj</Heading>
-                </Flex>
+        <Flex direction="column" style={{ height: "100%" }}>
+            <Box style={{ flex: 1, overflow: "hidden" }} m='5'>
+                <Card style={{ height: "100%", borderRadius: 0, minWidth: '300px' }}>
+                    <Flex direction="column" style={{ height: "100%" }} gap="3">
+                        <Flex
+                            direction="row"
+                            justify='center'
+                            align='center'
+                        >
+                            <Avatar
+                                src="icon.png"
+                                size='3'
+                                title="Mesaj icon"
+                                alt="Mesaj icon"
+                            />
+                            <Heading as="h1" ml='1'>Messaj</Heading>
+                        </Flex>
 
-                <TabsComponent options={tabsOptions} activeTab={activeTab} setActiveTab={setActiveTab} />
+                        <TabsComponent options={options} activeTab={activeTab} setActiveTab={setActiveTab} />
 
-                <Box style={{ flex: 1, overflow: "hidden" }}>
-                    <ScrollArea type="auto" scrollbars="vertical" style={{ height: "100%" }}>
-                        <Box pt="2">
-                            Szia
-                        </Box>
-                    </ScrollArea>
-                </Box>
-            </Flex>
-        </Card>
+                        {
+                            activeTab === 'chats' &&
+                            <Box style={{ flex: 1, overflow: "hidden" }}>
+                                <ScrollArea type="auto" scrollbars="vertical" style={{ height: "100%" }}>
+                                    <Box pt="2">
+                                        Csevegések
+                                    </Box>
+                                </ScrollArea>
+                            </Box>
+                        }
+
+                        {
+                            activeTab === 'friends' &&
+                            <Box style={{ flex: 1, overflow: "hidden" }}>
+                                <ScrollArea type="auto" scrollbars="vertical" style={{ height: "100%" }}>
+                                    <Box pt="2">
+                                        Ismerősök
+                                    </Box>
+                                </ScrollArea>
+                            </Box>
+                        }
+
+                         {
+                            activeTab === 'search' &&
+                            <Box style={{ flex: 1, overflow: "hidden" }}>
+                                <ScrollArea type="auto" scrollbars="vertical" style={{ height: "100%", userSelect: 'none', cursor: 'default' }}>
+                                    <Box pt="2" style={{ userSelect: 'none', cursor: 'default' }}>
+                                       <SearchNewFriendComponent handleSearchNewFriend={handleSearchNewFriend} />
+                                    </Box>
+                                </ScrollArea>
+                            </Box>
+                        }
+
+                    </Flex>
+                </Card>
+            </Box>
+        </Flex>
+
     );
 }
