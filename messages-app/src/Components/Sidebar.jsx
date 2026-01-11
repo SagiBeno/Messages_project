a27b8bc996952c08b366811159d35750ff831da1
SagiBeno <benosagi321@gmail.com>
2026-01-11 15:46:56 +0100

    Show incoming requests

--- messages-app/src/Components/Sidebar.jsx
+++ messages-app/src/Components/Sidebar.jsx
@@ -2,10 +2,10 @@ import { Box, Card, Flex, Tabs, Text, ScrollArea, Heading, Avatar, TextField, Sp
 import TabsComponent from "./TabsComponent";
 import SearchNewFriendComponent from "./SearchNewFriendComponent";
 import SearchCard from "./SearchCard";
+import IncomingRequestsCard from "./IncomingRequestsCard";
 import { useState } from "react";
 
-export default function Sidebar({ options, activeTab, setActiveTab, handleSearchNewFriend, cardsData, addFriend, loading, currentUserId }) {
-
+export default function Sidebar({ options, activeTab, setActiveTab, incomingRequests, handleSearchNewFriend, searchData, addFriend, loading, currentUserId }) {
     return (
         <Flex direction="column" style={{ height: "100%" }}>
             <Box style={{ flex: 1, overflow: "hidden" }} m='5'>
@@ -41,10 +41,34 @@ export default function Sidebar({ options, activeTab, setActiveTab, handleSearch
                         {
                             activeTab === 'friends' &&
                             <Box style={{ flex: 1, overflow: "hidden" }}>
-                                <ScrollArea type="auto" scrollbars="vertical" style={{ height: "100%" }}>
-                                    <Box pt="2">
-                                        Ismerősök
-                                    </Box>
+                                <ScrollArea type="auto" scrollbars="vertical" radius='full' style={{ height: "100%", paddingRight: '15px' }}>
+                                    {
+                                        loading 
+                                            ?
+                                                <Flex direction='column' justify='center' align='center' style={{ height: '90%' }}>
+                                                    <Spinner size='3' />
+                                                </Flex>
+                                            :
+                                                <Flex direction='column'>
+                                                    {
+                                                        (incomingRequests.length > 0) && (
+                                                        <>
+                                                            <Text as="p" style={{ opacity: '0.6',userSelect: 'none',cursor: 'default' }} mb='2'>
+                                                                Beérkező kérelmek
+                                                            </Text>
+
+                                                            {
+                                                                incomingRequests.map( (request, idx) => (<IncomingRequestsCard key={idx} request={request} />))
+                                                            }
+                                                        </>
+                                                    )}
+
+                                                    
+                                                    
+                                                </Flex>
+                                    }
+
+                                
                                 </ScrollArea>
                             </Box>
                         }
@@ -52,7 +76,7 @@ export default function Sidebar({ options, activeTab, setActiveTab, handleSearch
                         {
                             activeTab === 'search' &&
                             <Box style={{ flex: 1, overflow: "hidden" }}>
-                                <ScrollArea type="auto" scrollbars="vertical" style={{ height: "100%", userSelect: 'none', cursor: 'default' }}>
+                                <ScrollArea type="auto" scrollbars="vertical" radius='full' style={{ height: "100%", userSelect: 'none', cursor: 'default' }}>
                                     <Box pt="2" style={{ userSelect: 'none', cursor: 'default' }} mb='2'>
                                        <SearchNewFriendComponent handleSearchNewFriend={handleSearchNewFriend} />
                                     </Box>
@@ -65,11 +89,11 @@ export default function Sidebar({ options, activeTab, setActiveTab, handleSearch
                                     }
 
                                     {
-                                        cardsData.length > 0
+                                        searchData.length > 0
                                         &&
                                         <Flex direction='column'>
                                             {
-                                                cardsData.map( (user, idx) => <SearchCard key={idx} user={user} addFriend={addFriend} currentUserId={currentUserId} /> )
+                                                searchData.map( (user, idx) => <SearchCard key={idx} user={user} addFriend={addFriend} currentUserId={currentUserId} /> )
                                             }
                                         </Flex>
                                     }
