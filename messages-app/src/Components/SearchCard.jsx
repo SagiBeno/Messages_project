eb3519bfdf96c22745ba1fe56363f34fe27382fd
SagiBeno <benosagi321@gmail.com>
2026-01-11 15:35:35 +0100

    Add margin

--- messages-app/src/Components/SearchCard.jsx
+++ messages-app/src/Components/SearchCard.jsx
@@ -4,7 +4,7 @@ import { PlusIcon } from "@radix-ui/react-icons";
 export default function SearchCard( { user, currentUserId, addFriend } ) {
 
     return (
-        <Card>
+        <Card mb='2' >
             <Flex gap="3" align="center" direction="row" justify='between'>
 			    <Avatar
 			    	size="3"
