0c290842c4fe4f6f6aa02e4bbab3f981f8ea4c08
SagiBeno <benosagi321@gmail.com>
2026-01-14 18:09:31 +0100

    Refactor ChatComponent to utilize selectedFriend for dynamic display and improve structure

--- messages-app/src/Components/ChatComponent.jsx
+++ messages-app/src/Components/ChatComponent.jsx
@@ -1,36 +1,50 @@
-import { Box, Card, Text, ScrollArea, Avatar, IconButton, Tooltip } from "@radix-ui/themes";
+import { Box, Card, Text, ScrollArea, Avatar, IconButton, Tooltip, Flex } from "@radix-ui/themes";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import NewMessageComponent from "./NewMessageComponent";
 import { useState } from "react";
 
-export default function ChatComponent({ chat, newMessageContent, setNewMessageContent, handleSendMessage }) {
+export default function ChatComponent({ selectedChat, newMessageContent, setNewMessageContent, handleSendMessage, selectedFriend }) {
     
+    const selectedFriend_id = selectedFriend?.user_id;
+    const selectedFriend_fullName = selectedFriend?.full_name;
+
     return (
         <Box className="chatComponent">
             <Box style={{ height: "100%" }} className="chatCards">
 
-                <Box className="chatDataBox" >
-                    <Tooltip content="Csevegések">
-                        <IconButton radius="full" mr='1' color='tomato' id='hamburgerMenuIcon'>
-                            <HamburgerMenuIcon />
-                        </IconButton>
-                    </Tooltip>
+                {
+                    (selectedFriend_id && selectedFriend_fullName)
 
-                    <Avatar 
-                        fallback=''
-                        color='bronze'
-                        variant="soft"
-                        radius="full"
-                        size='2'
-                    />
-                    <Text as="p" size='3' style={{ margin: '0 auto', fontWeight: 'bold' }}>Név</Text>
-                </Box>
+                        ?
+                        <>
+                            <Box className="chatDataBox" >
 
-                <ScrollArea type="auto" scrollbars="vertical" mt='2'>
+                                <Avatar
+                                    fallback={`${selectedFriend_fullName.charAt(0)}${selectedFriend_fullName.split(' ')[1].charAt(0)}`}
+                                    color='bronze'
+                                    variant="soft"
+                                    radius="full"
+                                    size='2'
+                                />
+                                <Text as="p" size='3' style={{ margin: '0 auto', fontWeight: 'bold' }}>{selectedFriend_fullName}</Text>
+                            </Box>
 
-                </ScrollArea>
+                            <ScrollArea type="auto" scrollbars="vertical" mt='2'>
 
-                <NewMessageComponent newMessageContent={newMessageContent} setNewMessageContent={setNewMessageContent} handleSendMessage={handleSendMessage} />
+                            </ScrollArea>
+
+                            <NewMessageComponent newMessageContent={newMessageContent} setNewMessageContent={setNewMessageContent} handleSendMessage={handleSendMessage} />
+                        </>
+                        :
+
+                        <Flex p="4"
+                            justify='center'
+                            direction='column'
+                            style={{ height: '100%', textAlign: 'center' }}
+                        >
+                            Válasszon egy csevegést a bal oldali sávból, vagy kezdjen egy újat!
+                        </Flex>
+                }
             </Box>
         </Box>
     )
