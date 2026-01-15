96d4e46c36acad3082350d80052c6c70f01a254e
SagiBeno <benosagi321@gmail.com>
2026-01-15 10:30:00 +0100

    Add spinner

--- messages-app/src/App.jsx
+++ messages-app/src/App.jsx
@@ -5,28 +5,38 @@ import RegisterPage from './Pages/RegisterPage';
 import AppToast from './Components/Toast';
 import ChatsPage from './Pages/ChatsPage';
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Spinner, Flex } from '@radix-ui/themes';
 
 function App() {
   let navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-  const [toastData, setToastData] = useState( { open: false, title: '', description: '', isError: false } );
-  const [userData, setUserData] = useState( { isLoggedIn: false} );
+  const [toastData, setToastData] = useState({ open: false, title: '', description: '', isError: false });
+  const [userData, setUserData] = useState({ isLoggedIn: false });
 
-  useEffect( () => {
+  useEffect(() => {
     const storedUserData = JSON.parse(localStorage.getItem('userData'));
-    if ( storedUserData ) {
+    if (storedUserData) {
       setUserData(storedUserData);
     } else {
       navigate('/login')
     }
-  }, [] );
+  }, []);
 
   return (
     <>
       <Routes>
-        <Route path='/' element={ <ChatsPage loading={loading} setLoading={setLoading} userData={userData} toastData={toastData} setToastData={setToastData} setUserData={setUserData}/> } />
-        { !userData.isLoggedIn && <Route path='/register' element={<RegisterPage loading={loading} setLoading={setLoading} toastData={toastData} setToastData={setToastData} />} /> }
-        { !userData.isLoggedIn && <Route path='/login' element={ <LoginPage loading={loading} setLoading={setLoading} toastData={toastData} setToastData={setToastData} userData={userData} setUserData={setUserData} /> }/> }
+        <Route path='/' element={
+          userData.id
+            ?
+            <ChatsPage loading={loading} setLoading={setLoading} userData={userData} toastData={toastData} setToastData={setToastData} setUserData={setUserData} />
+            :
+            <Flex direction='column' justify='center' align='center' style={{ height: '100vh' }}>
+              <Spinner size='3' />
+            </Flex>
+        }
+        />
+        {!userData.isLoggedIn && <Route path='/register' element={<RegisterPage loading={loading} setLoading={setLoading} toastData={toastData} setToastData={setToastData} />} />}
+        {!userData.isLoggedIn && <Route path='/login' element={<LoginPage loading={loading} setLoading={setLoading} toastData={toastData} setToastData={setToastData} userData={userData} setUserData={setUserData} />} />}
       </Routes>
 
       <AppToast
